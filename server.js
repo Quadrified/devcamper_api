3be8a7b6a43aa835442f9dff4d4551d5d60117c2
Omer Quadri <omerquadri01@gmail.com>
2023-09-10 01:58:47 +0530

    Section 3: lesson 16

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Middleware
+const morgan = require('morgan');
+// const logger = require('./middleware/logger');
+
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Initialize Express App
 const app = express();
+
+// Mount Middlewares
+// app.use(logger);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
