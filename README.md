0e53c9cde53188692072f0a86d9e3d65175a65a7
Omer Quadri <omerquadri01@gmail.com>
2023-09-10 01:30:18 +0530

    Add upto Section 3, lesson 13

---
+++ README.md
@@ -0,0 +1,2 @@
+# Devcamper API
+---
--- server.js
+++ server.js
@@ -6,6 +6,42 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, error: null, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    error: null,
+    msg: `Showing bootcamps ${req.params.id}`,
+  });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, error: null, msg: 'Create new bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    error: null,
+    msg: `Update bootcamps ${req.params.id}`,
+  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    error: null,
+    msg: `Delete bootcamps ${req.params.id}`,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
