421e7a5fc1f01bbfe37cf3a65a1803ab0cac8cdb
Omer Quadri <omerquadri01@gmail.com>
2023-09-10 22:20:35 +0530

    Sec 4: lesson 18, 19, 20 MongoDB configuration.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Devcamper API
 ---
+## Steps to create and connect MongoDB:
+- Log in to MongoDB
+- Click Database > Create a cluster > Shared Cluster
+- After creating cluster
+  - Under Security 
+    - Database Access > Add a user
+      - Built-in Role > Read and Write from any DB
+    - Network Access
+      - Under IP Access List > Edit > Select Current IP Address > Confirm
+- Download MongoDB Compass and install
+- After installing Compass
+  - Inside MongoDB Atlas > Click Connect > Connect with Compass > Copy Connection String
+  - Paste Connection String in Compass > Save and Connect
+- Install mongoose
+  - Connect via ```db.js```
+```Note: From the Mongoose 6.0 onwards: useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.```
+
+## Add colors to console
+- Install ```colors.js``` and configure on ```server.js```
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Middlewares
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -18,12 +15,28 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Connect to MongoDB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handling unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
