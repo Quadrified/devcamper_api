2849c8e44aa9db250b9374d9b4b4a200ade9a0a0
Omer Quadri <omerquadri01@gmail.com>
2023-09-15 02:59:16 +0530

    Sec 6: lesson 35

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(
-    `MongoDB Connected: ${conn.connection.host}`.green.underline.bold
-  );
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,17 +12,42 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   let query;
 
-  let queryStr = JSON.stringify(req.query); // stringify because req.query is of type "object"
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
+  // Fields to exclude in filtering
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery); // stringify because reqQuery is of type "object"
+
+  // Create operators ($gt, $gte, etc.)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
-  console.log(queryStr);
-
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing query
   const bootcamps = await query;
 
   res.status(200).json({
